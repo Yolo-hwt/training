5fd8178942c2c67f111edc6634e72bf112c6a01f
Yolo-hwt <1803297996@qq.com>
2022-12-15 16:50:20 +0800

    api comment

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,14 @@
 import requests from "./request";
 
+/**
+ * 登陆部分接口
+ * @returns 
+ */
 //注册用户
 export const RegisterAdmin = (adminInfo) => requests({ url: '', data: adminInfo, method: 'post' });
 //鉴权是否为管理员
 export const AuthIsAdmin = (userNum) => requests({ url: "", data: userNum, method: 'post' });
 //登录
 export const AdminLoginIn = (adminInfo) => requests({ url: '', data: adminInfo, method: 'post' });
+
+
